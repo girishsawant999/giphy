ab501f6195f8ffdddd62451ec9cdff82517897f9
Girish Sawant <girishsawant999.gs@gmail.com>
2025-06-11 02:38:24 +0530

    Refactor Header component to improve logo visibility in dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,12 @@ const Header = () => {
     <header className="flex items-center justify-between p-4 ">
       <div className="flex items-center gap-2">
         <img src="/logo.svg" alt="Giphy logo" className="w-7 h-auto" />
-        <img src="/giphy.svg" alt="Giphy logo" />
+        <img src="/giphy.svg" alt="Giphy logo" className="hidden dark:block" />
+        <img
+          src="/giphy-dark.svg"
+          alt="Giphy logo"
+          className="block dark:hidden"
+        />
       </div>
     </header>
   );
