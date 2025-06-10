ca5ffd411bfbf3fc0a03ff44145ff75645030eb4
Girish Sawant <girishsawant999.gs@gmail.com>
2025-06-11 01:49:10 +0530

    Add Grid component for displaying trending GIFs with infinite scrolling

---
+++ src/components/Grid/index.tsx
@@ -0,0 +1,60 @@
+import LazyImage from "@/components/ImageLazy";
+import giphyService from "@/services/giphy";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+const perPage = 50;
+
+const Grid = () => {
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["trending-gifs"],
+      queryFn: ({ pageParam = 0 }) =>
+        giphyService.getTrendingGifs(perPage, pageParam),
+      getNextPageParam: (lastPage) => {
+        const nextOffset = lastPage.pagination.offset + perPage;
+        return nextOffset < lastPage.pagination.total_count
+          ? nextOffset
+          : undefined;
+      },
+      initialPageParam: 0,
+    });
+
+  return (
+    <>
+      <div className="columns-[200px]">
+        {data?.pages
+          .flatMap((page) => page.data)
+          .map((gif) => (
+            <LazyImage
+              className="mt-4"
+              key={gif.id}
+              src={gif.images.original.url}
+              alt={gif.title}
+              height={`${gif.images.original.height}px`}
+              sources={[
+                {
+                  type: "image/webp",
+                  srcset: gif.images.fixed_width.webp,
+                },
+              ]}
+            />
+          ))}
+      </div>
+      <div className="flex items-center justify-center mt-4">
+        {hasNextPage && (
+          <button
+            disabled={isFetchingNextPage}
+            aria-label="Load more GIFs"
+            title="Load more GIFs"
+            onClick={() => fetchNextPage()}
+            className="px-4 py-2 text-white bg-blue-500 rounded cursor-pointer"
+          >
+            Load More
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Grid;
