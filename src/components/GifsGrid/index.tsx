f4f6ca8998352ab1fef831b840ddef5738dea5a9
Girish Sawant <girishsawant999.gs@gmail.com>
2025-06-11 03:15:12 +0530

    Add GifsGrid component for displaying GIFs with lazy loading and pagination

---
+++ src/components/GifsGrid/index.tsx
@@ -0,0 +1,64 @@
+import LazyImage from "@/components/ImageLazy";
+
+type TProps = {
+  data: TGifData[];
+  hasNextPage: boolean;
+  fetchNextPage: () => void;
+  isFetchingNextPage: boolean;
+};
+
+const GifsGrid: React.FC<TProps> = ({
+  data,
+  hasNextPage,
+  fetchNextPage,
+  isFetchingNextPage,
+}) => {
+  return (
+    <>
+      <div className="columns-[200px] px-4 my-6">
+        {data.map((gif) => (
+          <LazyImage
+            containerClassName="mt-4 rounded-md"
+            key={gif.images.original.url}
+            src={gif.images.original.url}
+            alt={gif.title}
+            height={`200px`}
+            sources={[
+              {
+                type: "image/webp",
+                srcset: gif.images.fixed_width.webp,
+              },
+            ]}
+          />
+        ))}
+      </div>
+      <div className="flex items-center justify-center my-4">
+        {hasNextPage && (
+          <button
+            disabled={isFetchingNextPage}
+            aria-label="Load more GIFs"
+            title="Load more GIFs"
+            onClick={() => fetchNextPage()}
+            className="px-4 py-2 text-white bg-blue-500 flex items-center gap-1.5 justify-center rounded cursor-pointer hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+          >
+            {isFetchingNextPage && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                viewBox="0 0 256 256"
+                className="animate-spin group-hover:scale-110 transition-transform"
+              >
+                <path d="M140,32V64a12,12,0,0,1-24,0V32a12,12,0,0,1,24,0Zm84,84H192a12,12,0,0,0,0,24h32a12,12,0,0,0,0-24Zm-42.26,48.77a12,12,0,1,0-17,17l22.63,22.63a12,12,0,0,0,17-17ZM128,180a12,12,0,0,0-12,12v32a12,12,0,0,0,24,0V192A12,12,0,0,0,128,180ZM74.26,164.77,51.63,187.4a12,12,0,0,0,17,17l22.63-22.63a12,12,0,1,0-17-17ZM76,128a12,12,0,0,0-12-12H32a12,12,0,0,0,0,24H64A12,12,0,0,0,76,128ZM68.6,51.63a12,12,0,1,0-17,17L74.26,91.23a12,12,0,0,0,17-17Z"></path>
+              </svg>
+            )}
+            {isFetchingNextPage ? "Loading..." : "Load More"}
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default GifsGrid;
